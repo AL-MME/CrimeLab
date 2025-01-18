2d5ed1b720474aea1331d633d6b6585bfbcd2568
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-01-18 12:24:44 +0100

    add font

---
+++ crimelab-front/src/pages/seach.jsx
@@ -0,0 +1,11 @@
+import '../css/search.css';
+
+const Search = () => {
+    return (
+        <div className="search-background">
+            <h1 className='search-title'>Bienvenue sur CrimeLab</h1>
+        </div>
+    );
+}
+
+export default Search;
