4522541ae6b2e0d6bf9a0532fbc3e0e9aad50619
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-01-18 13:39:44 +0100

    commit pour enzo LOLLLL

--- crimelab-front/src/App.js
+++ crimelab-front/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Search from "./pages/seach";
+import Search from "./pages/search";
 
 function App() {
   return (
--- crimelab-front/src/pages/search.jsx
+++ crimelab-front/src/pages/search.jsx
@@ -1,9 +1,12 @@
+import SearchBar from '../components/searchBar';
 import '../css/search.css';
+
 
 const Search = () => {
     return (
         <div className="search-background">
             <h1 className='search-title'>Bienvenue sur CrimeLab</h1>
+            <SearchBar />
         </div>
     );
 }
