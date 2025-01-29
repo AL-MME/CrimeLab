ed595f92dcf4b5eb4d76422ef75db610b6115711
SpyKo_ <111909438+Spykoninho@users.noreply.github.com>
2025-01-29 23:17:01 +0100

    début de la manipulation

--- crimelab-front/src/components/NeovisVisualizer.jsx
+++ crimelab-front/src/components/NeovisVisualizer.jsx
@@ -1,38 +1,59 @@
 import React, { useEffect } from "react";
 import * as NeoVis from "neovis.js";
+import "../css/details.css";
 
 const NeoGraph = () => {
-  useEffect(() => {
-    const config = {
-      containerId: "viz",
-      neo4j: {
-        serverUrl: process.env.REACT_APP_NEO_URL,
-        serverUser: process.env.REACT_APP_NEO_USER,
-        serverPassword: process.env.REACT_APP_NEO_PWD,
-      },
-      labels: {
-        Person: {
-          label: "name",
-        },
-      },
-      relationships: {
-        KNOWS: {
-          thickness: "weight",
-        },
-      },
-      initialCypher: "MATCH (n)-[r]->(m) RETURN n,r,m",
-    };
+    useEffect(() => {
+        const config = {
+            containerId: "viz",
+            neo4j: {
+                serverUrl: process.env.REACT_APP_NEO_URL,
+                serverUser: process.env.REACT_APP_NEO_USER,
+                serverPassword: process.env.REACT_APP_NEO_PWD,
+            },
+            visConfig: {
+                edges: {
+                    arrows: {
+                        to: {
+                            enabled: true
+                        }
+                    },
+                },
+            },
+            labels: {
+                Person: {
+                    label: "firstname",
+                    [NeoVis.NEOVIS_ADVANCED_CONFIG]: {
+                        cypher: {
+                            value: "MATCH (n) RETURN n.firstname;"
+                        },
+                    }
+                },
+            },
+            relationships: {
+                KNOWS: {
+                    thickness: "weight",
+                },
+            },
+            initialCypher: "MATCH (n)-[r]-(m) RETURN n,r,m",
+        };
 
-    const viz = new NeoVis.default(config);
-    viz.render();
-  }, []);
+        const viz = new NeoVis.default(config);
 
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h2>Graph Neo4j avec NeoVis.js</h2>
-      <div id="viz" style={{ width: "800px", height: "600px", border: "1px solid black" }} />
-    </div>
-  );
+        viz.registerOnEvent("clickNode", (event) => {
+            if (event.node) {
+                console.log("Node clicked: ", event.node);
+            }
+        });
+
+        viz.render();
+    }, []);
+
+    return (
+        <div style={{ textAlign: "center", width: "100%", height: "100%" }}>
+            <div id="viz" style={{ width: "100%", height: "100%" }} />
+        </div>
+    );
 };
 
 export default NeoGraph;
--- crimelab-front/src/pages/details.jsx
+++ crimelab-front/src/pages/details.jsx
@@ -5,17 +5,23 @@ import NeoGraph from "../components/NeovisVisualizer";
 
 
 export const Details = () => {
-const { search } = useLocation();
-const queryParams = new URLSearchParams(search);
-const id = queryParams.get('id');
-const cat = queryParams.get('cat');
+    const { search } = useLocation();
+    const queryParams = new URLSearchParams(search);
+    const id = queryParams.get('id');
+    const cat = queryParams.get('cat');
 
-return (
-    <div>
-        <h1>Details Page</h1>
-        <p>ID: {id}</p>
-        <p>CAT: {cat}</p>
-        <NeoGraph />
-    </div>
-);
+    return (
+        <div className="details-background">
+            <div className="details-background-image">
+                <div className="details-navbar">
+                    <div className="details-navbar-title">
+                        <h1 className="crimeLabH1">CrimeLab</h1>
+                    </div>
+                </div>
+                <div className="details-content">
+                    <NeoGraph />
+                </div>
+            </div>
+        </div>
+    );
 };
